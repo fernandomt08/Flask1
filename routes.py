e6e58715a677477433d6533ea5821167bb715ef5
fmart087 <Vgy76yhN>
2016-11-23 19:33:00 -0500

    Test Push

--- routes.py
+++ routes.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+  return render_template("index.html")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+  app.run(debug=True)
